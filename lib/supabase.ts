64bab6de6fd83bca712469c98b0dc9a032172acd
Joseph Paderanga <paderanga.joseph03@gmail.com>
2026-01-30 13:49:56 +0800

    chore: added supabase helper

---
+++ lib/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Ensure the keys exist before trying to connect
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Missing Supabase Environment Variables');
+}
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
